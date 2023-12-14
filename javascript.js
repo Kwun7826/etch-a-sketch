c77e94a1dbc95c1c0eece3cbeaa1db5814add7ad
Kwun7826 <nguyenthanhlong7826@gmail.com>
2023-12-14 14:40:58 +0700

    Create the board function

--- javascript.js
+++ javascript.js
@@ -1,4 +1,18 @@
-const container = document.querySelector('container')
-for (i = 0 ; i < 256 ; i++){
-    const grid = document.createA
+
+document.addEventListener("DOMContentLoaded", function() {
+   createContainer(16);
+    console.log('hi');
+})
+
+function createContainer(size){
+    const container = document.querySelector('.container');
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    let numDivs = size * size;
+    for (i = 0 ; i < numDivs; i++){
+        const grid = document.createElement('div');
+        grid.style.backgroundColor = 'yellow';
+        container.appendChild(grid);
+    }
 }
+
